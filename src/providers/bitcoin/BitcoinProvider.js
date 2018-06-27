24f717c959b5107dd2784bbb1537507412117b4d
Harsh Vakharia <harshjv@gmail.com>
2018-06-27 18:42:10 +0530

    typo: time instead of timestamp

--- src/providers/bitcoin/BitcoinProvider.js
+++ src/providers/bitcoin/BitcoinProvider.js
@@ -24,7 +24,7 @@ export default class BitcoinProvider {
 BitcoinProvider.Block = {
   number: 'height',
   hash: 'hash',
-  timestamp: 'timestamp',
+  timestamp: 'time',
   difficulty: 'difficulty',
   size: 'size',
   parentHash: 'parentHash',
