d77b7ed18829ae833b6083b04019ed90eb938864
i3wgnit <i3wgnit@gmail.com>
2018-07-27 12:54:56 -0400

    Removed swap from providers They are in another branch ;-)

--- src/providers/index.js
+++ src/providers/index.js
@@ -1,22 +1,18 @@
 import BitcoinRPCProvider from './bitcoin/BitcoinRPCProvider'
 import BitcoinLedgerProvider from './bitcoin/BitcoinLedgerProvider'
-import BitcoinSwapProvider from './bitcoin/BitcoinSwapProvider'
 
 import EthereumRPCProvider from './ethereum/EthereumRPCProvider'
 import EthereumLedgerProvider from './ethereum/EthereumLedgerProvider'
 import EthereumMetaMaskProvider from './ethereum/EthereumMetaMaskProvider'
-import EthereumSwapProvider from './ethereum/EthereumSwapProvider'
 
 export default {
   bitcoin: {
     BitcoinRPCProvider,
-    BitcoinLedgerProvider,
-    BitcoinSwapProvider
+    BitcoinLedgerProvider
   },
   ethereum: {
     EthereumRPCProvider,
     EthereumLedgerProvider,
-    EthereumMetaMaskProvider,
-    EthereumSwapProvider
+    EthereumMetaMaskProvider
   }
 }
