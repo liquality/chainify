b734d9dcde5e2afeb5bb0ccf35068616adee750a
Matthew Black <mblackmblackmblack@gmail.com>
2018-08-01 16:42:32 -0400

    Add compressPubKey, pubKeyToHash160, pubKeyToAddress, addressToHash160

--- src/crypto.js
+++ src/crypto.js
@@ -1,9 +1,12 @@
-import sha256 from 'crypto-js/sha256'
-import ripemd160 from 'crypto-js/ripemd160'
-import basex from 'base-x'
+import { sha256, ripemd160 } from 'bcrypto'
+import base58 from 'bs58'
 import bech32 from 'bech32'
 
-const BASE58 = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+const networks = {
+  mainnet: '00',
+  testnet: '6F',
+  litecoin: '30'
+}
 
 const crypto = {
   /**
@@ -11,7 +14,7 @@ const crypto = {
    * @param {!string} message - any string.
    * @return {string} Returns the base58 of a string.
    */
-  base58: basex(BASE58),
+  base58,
 
   /**
    * Get bech32 of message.
@@ -36,17 +39,41 @@ const crypto = {
    * @param {!string} message - any string.
    * @return {string} Returns the sha256 of a string.
    */
-  sha256 (message) {
-    return sha256(message).toString()
-  },
+  sha256,
 
   /**
    * Get ripemd160 of message.
    * @param {!string} message - any string.
    * @return {string} Returns the ripemd160 of a string.
    */
-  ripemd160 (message) {
-    return ripemd160(message).toString()
+  ripemd160,
+
+  compressPubKey (pubKey) {
+    let x = pubKey.substring(2, 66)
+    let y = pubKey.substring(66, 130)
+    let prefix
+    let even = parseInt(y.substring(62, 64), 16) % 2 === 0
+    even ? prefix = '02' : prefix = '03'
+    return prefix + x
+  },
+
+  pubKeyToHash160 (pubKey) {
+    if (typeof pubKey === 'string') {
+      pubKey = Buffer.from(pubKey, 'hex')
+    }
+    return this.hash160(pubKey)
+  },
+
+  pubKeyToAddress (pubKey, network) {
+    let h160 = this.pubKeyToHash160(pubKey)
+    let prefixHash = Buffer.concat([Buffer.from(networks[network], 'hex'), h160])
+    let checksum = this.sha256(this.sha256(prefixHash)).slice(0, 4)
+    let addr = this.base58.encode(Buffer.concat([prefixHash, checksum]))
+    return addr
+  },
+
+  addressToHash160 (address) {
+    return this.base58.decode(address).toString('hex').substring(2, 42)
   }
 }
 
