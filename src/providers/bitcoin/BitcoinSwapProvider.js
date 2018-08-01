641abcf6428b008640e8c2ef305964332440a9fd
i3wgnit <i3wgnit@gmail.com>
2018-08-01 15:32:55 -0400

    Improved readability in generateSwap implementation

--- src/providers/bitcoin/BitcoinSwapProvider.js
+++ src/providers/bitcoin/BitcoinSwapProvider.js
@@ -2,19 +2,6 @@ import Provider from '../../Provider'
 
 export default class BitcoinSwapProvider extends Provider {
   generateSwap (recipientAddress, refundAddress, secretHash, expiration) {
-    const OPCODES = {
-      OP_DUP: '76',
-      OP_HASH160: 'a9',
-      OP_2SWAP: '72',
-      OP_IF: '63',
-      OP_EQUALVERIFY: '88',
-      OP_ELSE: '67',
-      OP_CHECKLOCKTIMEVERIFY: 'b1',
-      OP_2DROP: '6d',
-      OP_ENDIF: '68',
-      OP_CHECKSIG: 'ac'
-    }
-
     let expirationHex = expiration.toString(16)
     if (expirationHex.length % 2 === 1) {
       expirationHex = '0' + expirationHex
@@ -28,18 +15,32 @@ OP_DUP OP_HASH160
 OP_2SWAP
 OP_IF
   OP_HASH160
-  14${secretHash}
+  OP_PUSH20 ${secretHash}
   OP_EQUALVERIFY
-  14${recipientAddress}
+  OP_PUSH20 ${recipientAddress}
 OP_ELSE
   ${expirationHex.length.toString(16).padStart(2, '0')}
   ${expirationHex.join('')}
   OP_CHECKLOCKTIMEVERIFY
   OP_2DROP
-  14${refundAddress}
+  OP_PUSH20 ${refundAddress}
 OP_ENDIF
 OP_EQUALVERIFY OP_CHECKSIG
 `
+
+    const OPCODES = {
+      OP_DUP: '76',
+      OP_HASH160: 'a9',
+      OP_2SWAP: '72',
+      OP_IF: '63',
+      OP_PUSH20: '14',
+      OP_EQUALVERIFY: '88',
+      OP_ELSE: '67',
+      OP_CHECKLOCKTIMEVERIFY: 'b1',
+      OP_2DROP: '6d',
+      OP_ENDIF: '68',
+      OP_CHECKSIG: 'ac'
+    }
 
     return bytecode.match(/\S+/g)
       .map(op => op.startsWith('OP_') ? OPCODES[op] : op)
--- src/providers/ethereum/EthereumSwapProvider.js
+++ src/providers/ethereum/EthereumSwapProvider.js
@@ -10,14 +10,100 @@ export default class EthereumSwapProvider extends Provider {
     const expirationSize = expirationEncoded.length / 2
     const redeemDestinationEncoded = (redeemDestinationBase + expirationSize).toString(16)
     const refundDestinationEncoded = (refundDestinationBase + expirationSize).toString(16)
-    const expirationPushOpcode = (0x5f + expirationSize).toString(16)
     const dataSizeEncoded = (dataSizeBase + expirationSize).toString(16)
     const recipientAddressEncoded = recipientAddress.replace('0x', '') // Remove 0x if exists
     const refundAddressEncoded = refundAddress.replace('0x', '') // Remove 0x if exists
     const secretHashEncoded = secretHash.replace('0x', '') // Remove 0x if exists
-    return `60${dataSizeEncoded}80600b6000396000f36020806000803760218160008060026048f17f\
-${secretHashEncoded}602151141660${redeemDestinationEncoded}57\
-${expirationPushOpcode}${expirationEncoded}421160${refundDestinationEncoded}\
-57005b73${recipientAddressEncoded}ff5b73${refundAddressEncoded}ff`
+
+    const bytecode = `
+OP_PUSH1 ${dataSizeEncoded}
+OP_DUP1
+OP_PUSH1 0b
+OP_PUSH1 00
+OP_CODECOPY
+OP_PUSH1 00
+OP_RETURN
+
+
+
+OP_PUSH1 20
+
+OP_DUP1
+OP_PUSH1 00
+OP_DUP1
+OP_CALLDATACOPY
+
+
+OP_PUSH1 21
+OP_DUP2
+OP_PUSH1 00
+OP_DUP1
+OP_PUSH1 02
+OP_PUSH1 48
+OP_CALL
+
+OP_PUSH32 ${secretHashEncoded}
+OP_PUSH1 21
+OP_MLOAD
+OP_EQ
+
+OP_AND
+
+OP_PUSH1 ${redeemDestinationEncoded}
+OP_JUMPI
+
+
+OP_PUSH${expirationSize}
+${expirationEncoded}
+
+OP_TIMESTAMP
+OP_GT
+
+OP_PUSH1 ${refundDestinationEncoded}
+OP_JUMPI
+
+
+OP_STOP
+
+
+OP_JUMPDEST
+OP_PUSH20 ${recipientAddressEncoded}
+OP_SUICIDE
+
+
+OP_JUMPDEST
+OP_PUSH20 ${refundAddressEncoded}
+OP_SUICIDE
+`
+
+    const OPCODES = {
+      OP_PUSH: 0x60 - 1,
+      OP_DUP: 0x80 - 1,
+      OP_CODECOPY: 0x39,
+      OP_RETURN: 0xf3,
+      OP_CALLDATACOPY: 0x37,
+      OP_CALL: 0xf1,
+      OP_MLOAD: 0x51,
+      OP_EQ: 0x14,
+      OP_AND: 0x16,
+      OP_JUMPI: 0x57,
+      OP_TIMESTAMP: 0x42,
+      OP_GT: 0x11,
+      OP_STOP: 0x00,
+      OP_JUMPDEST: 0x5b,
+      OP_SUICIDE: 0xff
+    }
+
+    return bytecode.match(/\S+/g)
+      .map(op => {
+        if (op.startsWith('OP_')) {
+          const [opCode, mod] = op.match(/\D+|\d+/g)
+          return (OPCODES[opCode] + parseInt(mod || 0))
+            .toString(16)
+            .padStart(2, '0')
+        }
+        return op
+      })
+      .join('')
   }
 }
