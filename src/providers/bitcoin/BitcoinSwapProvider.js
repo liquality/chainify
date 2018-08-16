201570d2be7de80adc8e04519f1bedf14848ab13
i3wgnit <i3wgnit@gmail.com>
2018-08-16 10:06:15 -0400

    Add refund and redeem to bitcoin swap
    And also refactor generateSwap a bit.

--- src/providers/bitcoin/BitcoinSwapProvider.js
+++ src/providers/bitcoin/BitcoinSwapProvider.js
@@ -2,36 +2,72 @@ import Provider from '../../Provider'
 import { addressToPubKeyHash } from './BitcoinUtil'
 
 export default class BitcoinSwapProvider extends Provider {
+  _padHexStart (hex, length) {
+    let len = length || hex.length
+    len += len % 2
+
+    return hex.padStart(len, '0')
+  }
+
   generateSwap (recipientAddress, refundAddress, secretHash, expiration) {
-    let expirationHex = expiration.toString(16)
-    if (expirationHex.length % 2 === 1) {
-      expirationHex = '0' + expirationHex
-    }
-    expirationHex = expirationHex.match(/.{2}/g).reverse()
-    expirationHex.length = Math.min(expirationHex.length, 5)
-    expirationHex[expirationHex.length - 1] = '00'
+    let expirationHex = Buffer.from(this._padHexStart(expiration.toString(16)), 'hex').reverse()
+
+    expirationHex = expirationHex.slice(0, Math.min(expirationHex.length, 5))
+    expirationHex.writeUInt8(0, expirationHex.length - 1)
 
     const recipientPubKeyHash = addressToPubKeyHash(recipientAddress)
     const refundPubKeyHash = addressToPubKeyHash(refundAddress)
-    const expirationPushDataOpcode = expirationHex.length.toString(16).padStart(2, '0')
-    const expirationHexEncoded = expirationHex.join('')
+    const expirationPushDataOpcode = this._padHexStart(expirationHex.length.toString(16))
+    const expirationHexEncoded = expirationHex.toString('hex')
 
     return [
       '76', 'a9', // OP_DUP OP_HASH160
       '72', // OP_2SWAP
       '63', // OP_IF
       'a8', // OP_SHA256
-      '20', secretHash, // OP_PUSHDATA20 {secretHash}
+      '20', secretHash, // OP_PUSHDATA(20) {secretHash}
       '88', // OP_EQUALVERIFY
-      '14', recipientPubKeyHash, // OP_PUSHDATA20 {recipientPubKeyHash}
+      '14', recipientPubKeyHash, // OP_PUSHDATA(20) {recipientPubKeyHash}
       '67', // OP_ELSE
-      expirationPushDataOpcode, // OP_PUSHDATA{expirationHexLength}
+      expirationPushDataOpcode, // OP_PUSHDATA({expirationHexLength})
       expirationHexEncoded, // {expirationHexEncoded}
       'b1', // OP_CHECKLOCKTIMEVERIFY
       '6d', // OP_2DROP
-      '14', refundPubKeyHash, // OP_PUSHDATA20 {refundPubKeyHash}
+      '14', refundPubKeyHash, // OP_PUSHDATA(20) {refundPubKeyHash}
       '68', // OP_ENDIF
       '88', 'ac' // OP_EQUALVERIFY OP_CHECKSIG
     ].join('')
+  }
+
+  _spendSwap (signature, pubKey, isRedeem, secret) {
+    const redeemEncoded = isRedeem ? '51' : '00' // OP_1 : OP_0
+    const encodedSecret = isRedeem
+      ? [
+        this._padHexStart((secret.length / 2).toString(16)), // OP_PUSHDATA({secretLength})
+        secret
+      ]
+      : ['00'] // OP_0
+
+    const signaturePushDataOpcode = this._padHexStart((signature.length / 2).toString(16))
+    const pubKeyPushDataOpcode = this._padHexStart((pubKey.length / 2).toString(16))
+
+    const bytecode = [
+      signaturePushDataOpcode,
+      signature,
+      ...encodedSecret,
+      redeemEncoded,
+      pubKeyPushDataOpcode,
+      pubKey
+    ]
+
+    return bytecode.join('')
+  }
+
+  redeemSwap (pubKey, signature, secret) {
+    return this._spendSwap(signature, pubKey, true, secret)
+  }
+
+  refundSwap (pubKey, signature) {
+    return this._spendSwap(signature, pubKey, false)
   }
 }
