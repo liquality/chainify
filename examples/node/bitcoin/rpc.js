3d1d9908b8d153410c1d4674b76625ed373e701b
Harsh Vakharia <harshjv@gmail.com>
2018-09-29 19:12:24 +0530

    Remove bitcoin address from example

--- examples/node/bitcoin/rpc.js
+++ examples/node/bitcoin/rpc.js
@@ -6,5 +6,4 @@ bitcoin.addProvider(new BitcoinRPCProvider('http://localhost:18332', 'bitcoin', 
 
 ;(async () => {
   console.log(await bitcoin.generateBlock(1))
-  console.log(await bitcoin.getBalance('mgMuzuPNsCYPhw5NwktrtenaGYWLwps5FH'))
 })()
