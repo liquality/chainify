62a0b599ae5d4cde3379eb8a78378370f7d32401
Ubuntu <h4v0k@lilypad.ai2zrktiqoee5l0d1hsu0l0rnb.bx.internal.cloudapp.net>
2018-10-24 12:48:09 +0000

    fixed issue with txos counts and bitcore schema

--- src/providers/bitcoin/BitcoinLedgerProvider.js
+++ src/providers/bitcoin/BitcoinLedgerProvider.js
@@ -125,7 +125,7 @@ export default class BitcoinLedgerProvider extends LedgerProvider {
       }
 
       const utxos = await this.getMethod('getUnspentTransactions')(address.address)
-      const utxosValue = utxos.reduce((acc, utxo) => acc + (utxo.amount * 1e8), 0)
+      const utxosValue = utxos.reduce((acc, utxo) => ('satoshis' in utxo) ? acc + (utxo.satoshis) : acc + (utxo.amount * 1e8), 0)
 
       utxos.forEach((utxo) => {
         currentAmount += utxosValue
