dd7bd5b074617f130163d863f229353f8d8b2d17
monokh <mnokhb@gmail.com>
2018-10-24 10:41:37 +0100

    Fix linting

--- src/providers/bitcoin/BitcoinUtil.js
+++ src/providers/bitcoin/BitcoinUtil.js
@@ -71,11 +71,11 @@ function reverseBuffer (src) {
 
 function scriptNumSize (i) {
   return i > 0x7fffffff ? 5
-  : i > 0x7fffff ? 4
-  : i > 0x7fff ? 3
-  : i > 0x7f ? 2
-  : i > 0x00 ? 1
-  : 0
+    : i > 0x7fffff ? 4
+      : i > 0x7fff ? 3
+        : i > 0x7f ? 2
+          : i > 0x00 ? 1
+            : 0
 }
 
 function scriptNumEncode (number) {
@@ -97,7 +97,6 @@ function scriptNumEncode (number) {
 
   return buffer
 }
-
 
 export {
   compressPubKey,
