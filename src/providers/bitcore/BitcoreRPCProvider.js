c9dbe698e45fe228cf43c1494a314a24fe84371f
Alex Griessel <alex.griessel@gmail.com>
2018-10-24 12:51:21 +0200

    Added changes for bitcore

--- src/providers/bitcore/BitcoreRPCProvider.js
+++ src/providers/bitcore/BitcoreRPCProvider.js
@@ -1,7 +1,63 @@
+import { flatten } from 'lodash'
 import BitcoinRPCProvider from '../bitcoin/BitcoinRPCProvider'
 
 export default class Bitcore extends BitcoinRPCProvider {
+
+  async getBalance (addresses) {
+    addresses = addresses
+      .map(address => String(address))
+
+    const _utxos = await this.getUnspentTransactionsForAddresses(addresses)
+    const utxos = flatten(_utxos)
+    return utxos.reduce((acc, utxo) => acc + (utxo.satoshis), 0)
+  }
+
+  async getUnspentTransactionsForAddresses (addresses) {
+    return this.jsonrpc('getaddressutxos', { "addresses" : addresses })
+  }
+
+  async getUnspentTransactions (address) {
+    return this.jsonrpc('getaddressutxos', { "addresses" : [ address ] })
+  }
+
   async getAddressUtxos (addresses) {
     return this.jsonrpc('getaddressutxos', {'addresses': addresses})
   }
+
+  async getTransactionHex (transactionHash) {
+    return (await this.jsonrpc('getrawtransaction', transactionHash))
+  }
+
+  async decodeRawTransaction (rawTransaction) {
+    const data = await this.jsonrpc('decoderawtransaction', rawTransaction)
+    const { hash: txHash, txid: hash, vout } = data
+    const value = vout.reduce((p, n) => p + parseInt(n.value), 0)
+
+    console.log("raw tx", data)
+    const output = { hash, value, _raw: { hex: rawTransaction, data, txHash } }
+
+    return output
+  }
+
+  async getTransactionByHash (transactionHash) {
+    const rawTx = await this.getRawTransactionByHash(transactionHash)
+    const tx = await this.decodeRawTransaction(rawTx)
+    try {
+      const data = await this.jsonrpc('getrawtransaction', transactionHash, 1)
+      const { confirmations } = data
+      const output = Object.assign({}, tx, { confirmations })
+
+      if (confirmations > 0) {
+        const { blockhash: blockHash } = data
+        const { number: blockNumber } = await this.getBlockByHash(blockHash)
+        Object.assign(output, { blockHash, blockNumber })
+      }
+
+      return output
+    } catch (e) {
+      const output = Object.assign({}, tx)
+      return output
+    }
+  }
+
 }
