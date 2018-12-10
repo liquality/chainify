169a0de6102976c55882b723156a3b3108fb3e5a
Alex Griessel <alex.griessel@gmail.com>
2018-12-10 13:14:02 +0200

    Latest getaddresses

--- examples/swap/cli/getaddressesnew.js
+++ examples/swap/cli/getaddressesnew.js
@@ -5,16 +5,18 @@ const { BitcoinLedgerProvider, BitcoreRPCProvider } = providers.bitcoin
 
 const bitcoin = new Client()
 bitcoin.addProvider(new BitcoreRPCProvider('http://localhost:18332', 'bitcoin', 'local321'))
-bitcoin.addProvider(new BitcoinLedgerProvider({ network: networks.bitcoin, segwit: false }))
+bitcoin.addProvider(new BitcoinLedgerProvider({ network: networks.bitcoin_testnet, segwit: false }))
 //bitcoin.addProvider(new BitcoreRPCProvider('https://bitcoin.liquality.io/', 'liquality', 'liquality123'))
 // bitcoin.addProvider(new BitcoreRPCProvider('https://bitcoin.liquality.io/', 'liquality', 'liquality123'))
 
 ;(async () => {
   try {
-    // const [ address ] = await bitcoin.getAddresses(0, 100)
     // console.log(address)
     let d = Date.now()
     try {
+      const addresses = await bitcoin.getAddresses(0, 50)
+      console.log(addresses)
+      /*
       var xpubkeys = await bitcoin.getAddressExtendedPubKeys("49'/1'/0'")
       console.log(xpubkeys[0])
       var bjs = require("bitcoinjs-lib")
@@ -22,6 +24,7 @@ bitcoin.addProvider(new BitcoinLedgerProvider({ network: networks.bitcoin, segwi
       for ( var i = 0; i < 30; i++ ) { 
         console.log(node.derivePath("0/" + i).getAddress()); 
       }
+      */
       
       console.log('Time taken', `${(Date.now() - d) / 1000}s`)
     } catch (e) {
