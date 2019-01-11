e7d251b9b44da569a5b32a30512e3cca702a0bee
monokh <mnokhb@gmail.com>
2019-01-11 17:26:45 +0000

    BitcoinJsLibSwapProvider: fix redeeming and setting network

--- src/providers/bitcoin/BitcoinJsLibSwapProvider.js
+++ src/providers/bitcoin/BitcoinJsLibSwapProvider.js
@@ -13,6 +13,7 @@ export default class BitcoinJsLibSwapProvider extends Provider {
   constructor (chain = { network: networks.bitcoin }) {
     super()
     this._network = chain.network
+    this._bitcoinJsNetwork = this._network.isTestnet ? bitcoin.networks.testnet : bitcoin.networks.bitcoin
   }
 
   createSwapScript (recipientAddress, refundAddress, secretHash, expiration) {
@@ -51,7 +52,7 @@ export default class BitcoinJsLibSwapProvider extends Provider {
 
   async claimSwap (initiationTxHash, recipientAddress, refundAddress, secret, expiration) {
     const wif = await this.getMethod('dumpPrivKey')(recipientAddress)
-    const wallet = bitcoin.ECPair.fromWIF(wif, this._network)
+    const wallet = bitcoin.ECPair.fromWIF(wif, this._bitcoinJsNetwork)
     const secretHash = sha256(secret)
     const script = this.createSwapScript(recipientAddress, refundAddress, secretHash, expiration)
     const scriptPubKey = padHexStart(script)
@@ -68,17 +69,17 @@ export default class BitcoinJsLibSwapProvider extends Provider {
     vout.txid = initiationTxHash
     vout.vSat = vout.value * 1e8
     vout.script = Buffer.from(script, 'hex')
-    const walletRedeem = this.spendSwap(recipientAddress, wallet, secret, true, txfee, vout, regtest, expiration)
+    const walletRedeem = this.spendSwap(recipientAddress, wallet, secret, true, txfee, vout, this._bitcoinJsNetwork, expiration)
     return this.getMethod('sendRawTransaction')(walletRedeem)
   }
 
-  spendSwap (address, wallet, secret, isRedeem, txfee, vout, network, lockTime) {
+  spendSwap (address, wallet, secret, isRedeem, txfee, vout, network, expiration) {
     network = network || bitcoin.networks.bitcoin
     const hashType = bitcoin.Transaction.SIGHASH_ALL
 
     const txb = new bitcoin.TransactionBuilder(network)
 
-    if (lockTime) txb.setLockTime(lockTime)
+    if (!isRedeem) txb.setLockTime(expiration)
 
     txb.addInput(vout.txid, vout.n, 0)
     txb.addOutput(address, vout.vSat - txfee)
