0ab371969f4f10b1c0b76ae839ae82861bcf9d91
monokh <mnokhb@gmail.com>
2019-02-07 15:26:11 +0000

    Swap test: move id to chain object

--- test/integration/swap/chainToChain.js
+++ test/integration/swap/chainToChain.js
@@ -8,19 +8,19 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 
 chai.use(chaiAsPromised)
 
-async function testSwap (chain1Id, chain1, chain2Id, chain2) {
+async function testSwap (chain1, chain2) {
   console.log('\x1b[33m', `Generating secret: Watch for prompt`, '\x1b[0m')
-  const secret = await chain1.generateSecret('test')
+  const secret = await chain1.client.generateSecret('test')
   const secretHash = crypto.sha256(secret)
 
-  const chain1SwapParams = await getSwapParams(chain1Id, chain1)
-  const chain2SwapParams = await getSwapParams(chain2Id, chain2)
+  const chain1SwapParams = await getSwapParams(chain1)
+  const chain2SwapParams = await getSwapParams(chain2)
 
-  const chain1InitiationTxId = await initiateAndVerify(chain1, chain1Id, secretHash, chain1SwapParams)
-  const chain2InitiationTxId = await initiateAndVerify(chain2, chain2Id, secretHash, chain2SwapParams)
-  const revealedSecret = await claimAndVerify(chain1, chain1Id, chain1InitiationTxId, secret, chain1SwapParams)
+  const chain1InitiationTxId = await initiateAndVerify(chain1, secretHash, chain1SwapParams)
+  const chain2InitiationTxId = await initiateAndVerify(chain2, secretHash, chain2SwapParams)
+  const revealedSecret = await claimAndVerify(chain1, chain1InitiationTxId, secret, chain1SwapParams)
   expect(revealedSecret).to.equal(secret)
-  await claimAndVerify(chain2, chain2Id, chain2InitiationTxId, revealedSecret, chain2SwapParams)
+  await claimAndVerify(chain2, chain2InitiationTxId, revealedSecret, chain2SwapParams)
 }
 
 describe('Swap Chain to Chain', function () {
@@ -33,7 +33,7 @@ describe('Swap Chain to Chain', function () {
       console.log('\x1b[36m', 'Starting MetaMask connector on http://localhost:3333 - Open in browser to continue', '\x1b[0m')
       await metaMaskConnector.start()
       interval = setInterval(() => {
-        chains.bitcoinWithNode.generateBlock(1)
+        chains.bitcoinWithNode.client.generateBlock(1)
       }, 1000)
     })
 
@@ -43,11 +43,11 @@ describe('Swap Chain to Chain', function () {
     })
 
     it('BTC (Ledger) - ETH (MetaMask)', async () => {
-      await testSwap('Bitcoin Ledger', chains.bitcoinWithLedger, 'Ethereum MetaMask', chains.ethereumWithMetaMask)
+      await testSwap(chains.bitcoinWithLedger, chains.ethereumWithMetaMask)
     })
 
     it('ETH (MetaMask) - BTC (Ledger)', async () => {
-      await testSwap('Ethereum MetaMask', chains.ethereumWithMetaMask, 'Bitcoin Ledger', chains.bitcoinWithLedger)
+      await testSwap(chains.ethereumWithMetaMask, chains.bitcoinWithLedger)
     })
   })
 
@@ -56,7 +56,7 @@ describe('Swap Chain to Chain', function () {
 
     before(async () => {
       interval = setInterval(() => {
-        chains.bitcoinWithNode.generateBlock(1)
+        chains.bitcoinWithNode.client.generateBlock(1)
       }, 1000)
     })
 
@@ -65,11 +65,11 @@ describe('Swap Chain to Chain', function () {
     })
 
     it('BTC (Node) - ETH (Node)', async () => {
-      await testSwap('Bitcoin Node', chains.bitcoinWithNode, 'Ethereum Node', chains.ethereumWithNode)
+      await testSwap(chains.bitcoinWithNode, chains.ethereumWithNode)
     })
 
     it('ETH (Node) - BTC (Node)', async () => {
-      await testSwap('Ethereum Node', chains.ethereumWithNode, 'Bitcoin Node', chains.bitcoinWithNode)
+      await testSwap(chains.ethereumWithNode, chains.bitcoinWithNode)
     })
   })
 })
