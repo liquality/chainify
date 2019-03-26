20eed4b11ca81054df115fb7c0cc056fb221a331
Jules Dourlens <jdourlens@gmail.com>
2019-03-26 11:12:52 +0100

    ERC20 transfer

--- src/providers/ethereum/EthereumERC20Provider.js
+++ src/providers/ethereum/EthereumERC20Provider.js
@@ -1,4 +1,5 @@
 import Provider from '../../Provider'
+import { padHexStart } from '../../crypto'
 
 export default class EthereumERC20Provider extends Provider {
   constructor (contractAddress) {
@@ -6,7 +7,12 @@ export default class EthereumERC20Provider extends Provider {
     this._contractAddress = contractAddress
   }
 
-  erc20Transfer (to, amount) {
+  erc20Transfer (to, value) {
+    const functionSignature = 'a9059cbb'
+    const encodedAddress = padHexStart(to)
+    const encodedValue = padHexStart(value.toString(16))
+    const txBytecode = functionSignature + encodedAddress + encodedValue
+    return this.getMethod('sendTransaction')(this._contractAddress, 0, txBytecode)
   }
 
   erc20Balance (address) {
