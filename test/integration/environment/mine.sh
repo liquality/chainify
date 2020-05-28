fcc5547303b41516cf578760af4751dd322fb878
monokh <mnokhb@gmail.com>
2020-05-28 16:50:14 +0100

    Fix integration env

--- test/integration/environment/mine.sh
+++ test/integration/environment/mine.sh
@@ -1,5 +1,7 @@
 apk add curl curl-dev
 
+sleep 10 # Wait for wallet to load
+
 mine () { curl --basic -u bitcoin:local321 -X POST -d "{\"jsonrpc\": \"2.0\", \"id\": \"0\", \"method\": \"generate\", \"params\": [$1]}" http://${TARGET_HOST}:${TARGET_PORT} > /dev/null; }
 
 mine 105 # Extra few blocks for some mature funds
