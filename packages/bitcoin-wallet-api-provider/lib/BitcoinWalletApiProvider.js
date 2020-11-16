e1d025b5569dc4167ffbc33578d10dd7dcbbf45f
monokh <mnokhb@gmail.com>
2020-11-16 21:46:01 +0000

    lint

--- packages/bitcoin-wallet-api-provider/lib/BitcoinWalletApiProvider.js
+++ packages/bitcoin-wallet-api-provider/lib/BitcoinWalletApiProvider.js
@@ -9,19 +9,19 @@ export default class BitcoinWalletApiProvider extends BitcoinWalletProvider(Wall
     super(network, addressType, [network])
   }
 
-  async request(method, ...params) {
-    await window.bitcoin.enable();
-    return window.bitcoin.request({method, params})
+  async request (method, ...params) {
+    await window.bitcoin.enable()
+    return window.bitcoin.request({ method, params })
   }
 
-  async getWalletAddress(address) {
+  async getWalletAddress (address) {
     const walletAddress = await super.getWalletAddress(address)
     walletAddress._publicKey = Buffer.from(walletAddress.publicKey.data) // TODO: this shenanigans
     return new Address(walletAddress)
   }
-  
+
   async getAddresses (index = 0, num = 1, change = false) {
-    const addrs = await this.request('wallet_getAddresses', index, num, change);
+    const addrs = await this.request('wallet_getAddresses', index, num, change)
     return addrs.map(a => new Address(a))
   }
 
@@ -32,7 +32,7 @@ export default class BitcoinWalletApiProvider extends BitcoinWalletProvider(Wall
   async sendTransaction (to, value) {
     return this.request('wallet_sendTransaction', to, value)
   }
-  
+
   async signPSBT (data, input, address) {
     return this.request('wallet_signPSBT', data, input, address)
   }
