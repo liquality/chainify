8af3dcf55d75c43141a14c5fc0d7dacf9d9c2054
kraikov <raykov@weichain.tech>
2021-01-27 19:18:51 +0200

    Add near networks

---
+++ packages/near-networks/lib/index.js
@@ -0,0 +1,42 @@
+import {version} from '../package.json'
+
+export default {
+  mainnet: {
+    name: 'mainnet',
+    networkId: 'mainnet',
+    nodeUrl: 'https://rpc.mainnet.near.org',
+    walletUrl: 'https://wallet.near.org',
+    helperUrl: 'https://helper.mainnet.near.org',
+    explorerUrl: 'https://explorer.mainnet.near.org',
+    coinType: '397',
+    isTestnet: false,
+  },
+  testnet: {
+    name: 'testnet',
+    networkId: 'testnet',
+    nodeUrl: 'https://rpc.testnet.near.org',
+    walletUrl: 'https://wallet.testnet.near.org',
+    helperUrl: 'https://helper.testnet.near.org',
+    explorerUrl: 'https://explorer.testnet.near.org',
+    isTestnet: true,
+  },
+  betanet: {
+    name: 'betanet',
+    networkId: 'betanet',
+    nodeUrl: 'https://rpc.betanet.near.org',
+    walletUrl: 'https://wallet.betanet.near.org',
+    helperUrl: 'https://helper.betanet.near.org',
+    explorerUrl: 'https://explorer.betanet.near.org',
+    isTestnet: true,
+  },
+  local: {
+    name: 'local',
+    networkId: 'local',
+    nodeUrl: 'http://localhost:3030',
+    keyPath: `${process.env.HOME}/.near/validator_key.json`,
+    walletUrl: 'http://localhost:4000/wallet',
+    isTestnet: true,
+  },
+
+  version,
+}
