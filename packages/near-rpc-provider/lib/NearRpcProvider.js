0cc869fb9a4c2ec8377fc971ed40f3d97be54388
Krasimir Raykov <raykov@weichain.tech>
2021-02-02 12:08:55 +0200

    Implement: - sendRawTransaction - getBlockHeight - getGasPrice - getBalance - isAddressUsed
    To be implemented:
    - assertContractExists
    - getTransactionReceipt
    - getTransactionByHash
    - getBlockByHash
    - getBlockByNumber

--- packages/near-rpc-provider/lib/NearRpcProvider.js
+++ packages/near-rpc-provider/lib/NearRpcProvider.js
@@ -1,36 +1,82 @@
+import { get, isArray } from 'lodash'
+import { BigNumber } from 'bignumber.js'
+
 import JsonRpcProvider from '@liquality/jsonrpc-provider'
 
-import {version} from '../package.json'
+import { version } from '../package.json'
 
 export default class NearRpcProvider extends JsonRpcProvider {
-  constructor(uri) {
-    super(uri, username, password)
+  constructor (uri) {
+    super(uri)
     this._usedAddressCache = {}
   }
 
-  async rpc(method, ...params) {}
+  async rpc (method, params) {
+    const id = Date.now()
+    const req = { id, method, params, jsonrpc: '2.0' }
+    const data = await this.nodePost('', req)
+    return this._parseResponse(data)
+  }
 
-  async sendRawTransaction(hash) {}
+  async sendRawTransaction (hash) {
+    return this.rpc('broadcast_tx_commit', [hash])
+  }
 
-  async getBlockByHash(blockHash, includeTx) {}
+  async getBlockByHash (blockHash, includeTx) {}
 
-  async getBlockByNumber(blockNumber, includeTx) {}
+  async getBlockByNumber (blockNumber, includeTx) {}
 
-  async getBlockHeight() {}
+  async getBlockHeight () {
+    const result = await this.rpc('block', { finality: 'final' })
+    return get(result, 'header.height')
+  }
 
-  async getTransactionByHash(txHash) {}
+  async getTransactionByHash (txHash) {}
 
-  async getTransactionReceipt(txHash) {}
+  async getTransactionReceipt (txHash) {}
 
-  async getGasPrice() {}
+  async getGasPrice () {
+    const result = await this.rpc('gas_price', [null])
+    return get(result, 'gas_price')
+  }
 
-  async getBalance(addresses) {}
+  async getBalance (addresses) {
+    if (!isArray(addresses)) {
+      addresses = [addresses]
+    }
 
-  async estimateGas(transaction) {}
+    const promiseBalances = await Promise.all(
+      addresses.map((address) =>
+        this.rpc('query', {
+          request_type: 'view_account',
+          finality: 'final',
+          account_id: address
+        })
+      )
+    )
 
-  async isAddressUsed(address) {}
+    return promiseBalances
+      .map((balance) => {
+        const costPerByte = new BigNumber(1)
+        const stateStaked = new BigNumber(balance.storage_usage).multipliedBy(
+          costPerByte
+        )
+        const staked = new BigNumber(balance.locked)
+        const totalBalance = new BigNumber(balance.amount).plus(staked)
+        const availableBalance = totalBalance.minus(
+          BigNumber.max(staked, stateStaked)
+        )
+        return new BigNumber(availableBalance)
+      })
+      .reduce((acc, balance) => acc.plus(balance), new BigNumber(0))
+  }
 
-  async assertContractExists(address) {}
+  async isAddressUsed (address) {
+    const activity = await this.getMethod('getAccountActivity')(address)
+    return activity.length > 0
+  }
+
+  async assertContractExists (address) {}
 }
 
-EthereumRpcProvider.version = version
+NearRpcProvider.version = version
---
+++ packages/near-rpc-provider/test/unit/index.test.js
@@ -0,0 +1,41 @@
+/* eslint-env mocha */
+import chai from 'chai'
+
+import Client from '../../../client/lib'
+import NearRpcProvider from '../../lib'
+
+chai.use(require('chai-bignumber')())
+chai.config.truncateThreshold = 0
+
+describe('Near RPC provider', () => {
+  let client
+  let provider
+
+  beforeEach(() => {
+    client = new Client()
+    provider = new NearRpcProvider('https://rpc.testnet.near.org')
+    client.addProvider(provider)
+  })
+
+  describe('getBlockHeight', () => {
+    it('should return block height', async () => {
+      const height = await client.chain.getBlockHeight()
+      console.log('height ', height)
+    })
+  })
+
+  describe('getBalance', () => {
+    it('should return correct balance', async () => {
+      const balance = await client.chain.getBalance(['krasi'])
+      console.log('balance: ', balance)
+    })
+  })
+
+  describe('getGasPrice', () => {
+    it('should return correct balance', async () => {
+      console.log(client.chain)
+      const gasPrice = await provider.getGasPrice()
+      console.log('gas price: ', gasPrice)
+    })
+  })
+})
