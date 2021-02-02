6634c1b02770c1dd0037ed77ff5e26460def0ee8
Krasimir Raykov <raykov@weichain.tech>
2021-02-02 15:51:09 +0200

    Implement: - getAddresses - getUnusedAddress - getUsedAddresses - signMessage - isWalletAvailable - getWalletNetworkId - canUpdateFee
    To be implemented:
    - sendTransaction
    - sendSweepTransaction

--- packages/near-js-wallet-provider/lib/NearJsWalletProvider.js
+++ packages/near-js-wallet-provider/lib/NearJsWalletProvider.js
@@ -1,27 +1,62 @@
 import Provider from '@liquality/provider'
+import { Address } from '@liquality/utils'
+import { InMemorySigner, KeyPair } from 'near-api-js'
+import { InMemoryKeyStore } from 'near-api-js/lib/key_stores'
+import { parseSeedPhrase } from 'near-seed-phrase'
 
-import {version} from '../package.json'
+import { version } from '../package.json'
 
-class NearJsWalletProvider extends Provider {
-  constructor(network, mnemonic) {
+export default class NearJsWalletProvider extends Provider {
+  constructor (network, mnemonic) {
+    super()
     this._network = network
     this._mnemonic = mnemonic
     this._derivationPath = `m/44'/${network.coinType}'/0'`
+    this._keyStore = new InMemoryKeyStore()
   }
 
-  async getAddresses() {}
+  async getAddresses () {
+    const { publicKey, secretKey } = parseSeedPhrase(
+      this._mnemonic,
+      this._derivationPath
+    )
 
-  async getUnusedAddress() {}
+    const keyPair = KeyPair.fromString(secretKey)
+    const address = await this.getMethod('getAccounts')(publicKey, 0)
+    await this._keyStore.setKey(this._network.networkId, address, keyPair)
 
-  async getUsedAddresses() {}
+    return [new Address(address, this._derivationPath, publicKey, 0)]
+  }
 
-  async signMessage(message) {}
+  async getUnusedAddress () {
+    const addresses = await this.getAddresses()
+    return addresses[0]
+  }
 
-  async sendTransaction(to, value, data, _gasPrice) {}
+  async getUsedAddresses () {
+    return this.getAddresses()
+  }
 
-  async sendSweepTransaction(address, _gasPrice) {}
+  async signMessage (message) {
+    return new InMemorySigner(this._keyStore).signMessage(Buffer.from(message))
+  }
 
-  async updateTransactionFee(tx, newGasPrice) {}
+  async sendTransaction (to, value, data, _gasPrice) {}
+
+  async sendSweepTransaction (address, _gasPrice) {}
+
+  async isWalletAvailable () {
+    const addresses = await this.getAddresses()
+    return addresses.length > 0
+  }
+
+  async getWalletNetworkId () {
+    return this._network.networkId
+  }
+
+  canUpdateFee () {
+    return false
+  }
 }
 
 NearJsWalletProvider.version = version
