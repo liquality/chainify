23f5a2a5eff7b00f124b0f76c5adab00d6bdb5c0
Krasimir Raykov <raykov@weichain.tech>
2021-04-14 23:24:39 +0300

    update: near-rpc-provider unit tests update

--- packages/near-rpc-provider/test/unit/index.test.ts
+++ packages/near-rpc-provider/test/unit/index.test.ts
@@ -1,53 +1,40 @@
 /* eslint-env mocha */
-import chai, { expect } from "chai";
+import chai, { expect } from 'chai'
 
-import Client from "../../../client/lib";
-import NearRpcProvider from "../../lib";
+import Client from '../../../client/lib'
+import NearRpcProvider from '../../lib'
+import NearNetwork from '../../../near-networks'
+import { BigNumber } from '../../../types/lib'
 
-chai.config.truncateThreshold = 0;
+chai.config.truncateThreshold = 0
 
-describe("Near RPC provider", () => {
-  let client: Client;
-  let provider: NearRpcProvider;
+describe('Near RPC provider', () => {
+  let client: Client
+  let provider: NearRpcProvider
 
   beforeEach(() => {
-    client = new Client();
-    provider = new NearRpcProvider({
-      networkId: "testnet",
-      nodeUrl: "https://rpc.testnet.near.org",
-      walletUrl: "https://wallet.testnet.near.org",
-      helperUrl: "https://helper.testnet.near.org",
-      explorerUrl: "https://explorer.testnet.near.org",
-    });
-    client.addProvider(provider);
-  });
+    client = new Client()
+    provider = new NearRpcProvider(NearNetwork.near_testnet)
+    client.addProvider(provider)
+  })
 
-  describe("getBlockHeight", () => {
-    it("should return block height", async () => {
-      const height = await client.chain.getBlockHeight();
-      console.log("height ", height);
-    });
-  });
+  describe('getBlockHeight', () => {
+    it('should return block height', async () => {
+      await client.chain.getBlockHeight()
+    })
+  })
 
-  describe("getBalance", () => {
-    it("should return correct balance", async () => {
-      const balance = await client.chain.getBalance(["krasi"]);
-      console.log("balance: ", balance);
-    });
-  });
+  describe('getBalance', () => {
+    it('should return correct balance', async () => {
+      const balance = await client.chain.getBalance(['krasi'])
+      expect(BigNumber.isBigNumber(balance)).to.be.true
+    })
+  })
 
-  describe("getBalance", () => {
-    it("should return correct balance", async () => {
-      const balance = await client.chain.getBalance(["non-existing-account"]);
-      expect(balance.toString()).equal("0");
-    });
-  });
-
-  describe("getGasPrice", () => {
-    it("should return correct balance", async () => {
-      console.log(client.chain);
-      const gasPrice = await provider.getGasPrice();
-      console.log("gas price: ", gasPrice);
-    });
-  });
-});
+  describe('getBalance', () => {
+    it('should return correct balance for non existing account', async () => {
+      const balance = await client.chain.getBalance(['non-existing-account'])
+      expect(balance.eq(0)).to.be.true
+    })
+  })
+})
