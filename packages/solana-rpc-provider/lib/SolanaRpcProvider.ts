3b7ce7978a8a472786844aa50b860f40dd678dc9
AndonMitev <mitevandon94@gmail.com>
2021-07-05 12:08:22 +0300

    Add new line in bytecode

--- packages/solana-rpc-provider/lib/SolanaRpcProvider.ts
+++ packages/solana-rpc-provider/lib/SolanaRpcProvider.ts
@@ -121,7 +121,9 @@ export default class SolanaRpcProvider extends NodeProvider implements Partial<C
     return await this.connection.sendRawTransaction(wireTransaciton)
   }
 
-  async getProgramAccounts(programId: PublicKey): Promise<
+  async getProgramAccounts(
+        programId: PublicKey
+          ): Promise<
     {
       pubkey: PublicKey
       account: AccountInfo<Buffer>
