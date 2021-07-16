2921e07be0318fa388e0eab6db4ddf4390091afa
AndonMitev <mitevandon94@gmail.com>
2021-07-17 01:22:18 +0300

    feat/implement constructor in terra wallet

--- packages/terra-wallet-provider/lib/TerraWalletProvider.ts
+++ packages/terra-wallet-provider/lib/TerraWalletProvider.ts
@@ -1,6 +1,28 @@
 import { WalletProvider } from '@liquality/wallet-provider'
 import { Address } from '@liquality/types'
+import { TerraNetwork } from '@liquality/terra-networks'
+
+interface TerraWalletProviderOptions {
+  network: TerraNetwork
+  mnemonic: string
+  derivationPath: string
+}
+
 export default class TerraWalletProvider extends WalletProvider {
+  _network: TerraNetwork
+  _mnemonic: string
+  _derivationPath: string
+  _addressCache: { [key: string]: Address }
+
+  constructor(options: TerraWalletProviderOptions) {
+    const { network, mnemonic, derivationPath } = options
+    super({ network })
+    this._network = network
+    this._mnemonic = mnemonic
+    this._derivationPath = derivationPath
+    this._addressCache = {}
+  }
+
   isWalletAvailable(): Promise<boolean> {
     throw new Error('Method not implemented.')
   }
