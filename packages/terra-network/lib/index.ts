a39ef93407cf5e0d07a7a75376b5b92d3d44f43f
AndonMitev <mitevandon94@gmail.com>
2021-07-16 16:09:47 +0300

    feat: Init terra networks

--- packages/terra-network/lib/index.ts
+++ packages/terra-network/lib/index.ts
@@ -1,3 +1,35 @@
-import { Network } from "@liquality/types";
+import { Network } from '@liquality/types'
+
 export interface TerraNetwork extends Network {
+  networkId: string
+  nodeUrl: string
+  helperUrl: string
+  chainID: string
 }
+
+const terra_mainnet: TerraNetwork = {
+  name: 'mainnet',
+  networkId: 'mainnet',
+  nodeUrl: 'https://lcd.terra.dev',
+  helperUrl: 'https://finder.terra.money',
+  coinType: '397',
+  isTestnet: false,
+  chainID: 'columbus-4'
+}
+
+const terra_testnet: TerraNetwork = {
+  name: 'testnet',
+  networkId: 'testnet',
+  nodeUrl: 'https://tequila-lcd.terra.dev',
+  helperUrl: 'https://finder.terra.money/tequila-0004',
+  coinType: '397',
+  isTestnet: true,
+  chainID: 'tequila-0004'
+}
+
+const TerraNetworks = {
+  terra_mainnet,
+  terra_testnet
+}
+
+export { TerraNetworks }
