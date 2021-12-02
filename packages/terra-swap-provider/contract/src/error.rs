70c2c4e0731df31445001937b9d2a8b4cb43fa73
AndonMitev <mitevandon94@gmail.com>
2021-12-02 14:11:29 +0200

    fix: Apply pr comments

--- packages/terra-swap-provider/contract/src/error.rs
+++ packages/terra-swap-provider/contract/src/error.rs
@@ -16,7 +16,6 @@ pub enum ContractError {
     QueryNotImplemented {},
     #[error("Invalid amount of coins")]
     InvalidAmountOfCoins {},
-    
     // Add any other custom errors you like here.
     // Look at https://docs.rs/thiserror/1.0.21/thiserror/ for details.
 }
