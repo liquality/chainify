48c2660a02b700e024c0642fe6fbfcff98f5f7df
Slav Keremidchiev <skeremidch@gmail.com>
2022-01-13 12:28:50 +0200

    feat: further extend and improve nft interface

--- packages/client/lib/Nft.ts
+++ packages/client/lib/Nft.ts
@@ -8,7 +8,7 @@ export default class Nft implements NftProvider {
   }
 
   /** @inheritdoc */
-  async balance(contract: Address | string, tokenIDs?: BigNumber | BigNumber[]): Promise<BigNumber | BigNumber[]> {
+  async balance(contract: Address | string, tokenIDs?: BigNumber[]): Promise<BigNumber | BigNumber[]> {
     const balance = await this.client.getMethod('balance')(contract, tokenIDs)
     return balance
   }
@@ -17,25 +17,41 @@ export default class Nft implements NftProvider {
   async transfer(
     contract: Address | string,
     receiver: Address | string,
-    tokenIds: BigNumber | BigNumber[],
+    tokenIDs: BigNumber | BigNumber[],
     values?: BigNumber[]
   ): Promise<Transaction> {
-    const transaction = await this.client.getMethod('transfer')(contract, receiver, tokenIds, values)
+    const transaction = await this.client.getMethod('transfer')(contract, receiver, tokenIDs, values)
     this.client.assertValidTransaction(transaction)
     return transaction
   }
 
   /** @inheritdoc */
-  async approve(contract: Address | string, receiver: Address | string, tokenId: BigNumber): Promise<Transaction> {
-    const transaction = await this.client.getMethod('approve')(contract, receiver, tokenId)
+  async approve(contract: Address | string, operator: Address | string, tokenID: BigNumber): Promise<Transaction> {
+    const transaction = await this.client.getMethod('approve')(contract, operator, tokenID)
     this.client.assertValidTransaction(transaction)
     return transaction
   }
 
   /** @inheritdoc */
-  async approveAll(contract: Address | string, receiver: Address | string, state?: boolean): Promise<Transaction> {
-    const transaction = await this.client.getMethod('approveAll')(contract, receiver, state)
+  async isApproved(contract: Address | string, tokenID: BigNumber): Promise<Address> {
+    const operator = await this.client.getMethod('isApproved')(contract, tokenID)
+    return operator
+  }
+
+  /** @inheritdoc */
+  async approveAll(contract: Address | string, operator: Address | string, state?: boolean): Promise<Transaction> {
+    const transaction = await this.client.getMethod('approveAll')(contract, operator, state)
     this.client.assertValidTransaction(transaction)
     return transaction
+  }
+
+  /** @inheritdoc */
+  async isApprovedForAll(
+    contract: Address | string,
+    owner: Address | string,
+    operator: Address | string
+  ): Promise<boolean> {
+    const state = await this.client.getMethod('isApprovedForAll')(contract, owner, operator)
+    return state
   }
 }
--- packages/types/lib/nft.ts
+++ packages/types/lib/nft.ts
@@ -9,40 +9,57 @@ export interface NftProvider {
    * @param {BigNumber} [tokenIDs] - NFT ids for ERC1155 standard.
    * @return {BigNumber | BigNumber[]} Resolves with in case of ERC721 - amount of NFTs, in case of ERC1155 - amount of copies owned from a NFT with specified id.
    */
-  balance(contract: Address | string, tokenIDs?: BigNumber | BigNumber[]): Promise<BigNumber | BigNumber[]>
+  balance(contract: Address | string, tokenIDs?: BigNumber[]): Promise<BigNumber | BigNumber[]>
 
   /**
    * transfer NFT (ERC721 & ERC1155)
    * @param {!string} contract - NFT contract address.
    * @param {!string} receiver - Transfer to address.
-   * @param {!BigNumber} tokenIds - Token IDs.
+   * @param {!BigNumber} tokenIDs - Token IDs.
    * @param {BigNumber[]} [values] - Amount of NFT copies to be transferred from each tokenID
    * @return {Promise<Transaction>} Resolves with a signed transaction.
    */
   transfer(
     contract: Address | string,
     receiver: Address | string,
-    tokenIds: BigNumber | BigNumber[],
+    tokenIDs: BigNumber | BigNumber[],
     values?: BigNumber[]
   ): Promise<Transaction>
 
   /**
    * approve NFT (ERC721)
    * @param {!string} contract - NFT contract address.
-   * @param {!string} receiver - Approve to address.
-   * @param {!BigNumber} tokenId - Token IDs.
+   * @param {!string} operator - Approve to address.
+   * @param {!BigNumber} tokenID - Token IDs.
    * @return {Promise<Transaction>} Resolves with a signed transaction.
    */
-  approve(contract: Address | string, receiver: Address | string, tokenId: BigNumber): Promise<Transaction>
+  approve(contract: Address | string, operator: Address | string, tokenID: BigNumber): Promise<Transaction>
+
+  /**
+   * check operator of NFT (ERC721)
+   * @param {!string} contract - NFT contract address.
+   * @param {!BigNumber} tokenID - Token IDs.
+   * @return {Promise<Address>} Operator address.
+   */
+  isApproved(contract: Address | string, tokenID: BigNumber): Promise<Address>
 
   /**
    * approve all NFTs (ERC721 & ERC1155)
    * @param {!string} contract - NFT contract address.
-   * @param {!string} receiver - Approve to address.
+   * @param {!string} operator - Approve to address.
    * @param {boolean} [state] - Give (true) or take (false) approval, give approval by default.
    * @return {Promise<Transaction>} Resolves with a signed transaction.
    */
-  approveAll(contract: Address | string, receiver: Address | string, state?: boolean): Promise<Transaction>
+  approveAll(contract: Address | string, operator: Address | string, state?: boolean): Promise<Transaction>
+
+  /**
+   * approve all NFTs (ERC721 & ERC1155)
+   * @param {!string} contract - NFT contract address.
+   * @param {!string} owner - Approve to address.
+   * @param {!string} operator - Approve to address.
+   * @return {Promise<boolean>}
+   */
+  isApprovedForAll(contract: Address | string, owner: Address | string, operator: Address | string): Promise<boolean>
 
   // TODO:
   //   /**
