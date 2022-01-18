91051b902caa88bb25b71f54d311d57d40f0f4bf
Slav Keremidchiev <skeremidch@gmail.com>
2022-01-18 18:11:43 +0200

    feat: NftErc1155Provider extending NftBaseProvider

--- packages/nft-erc1155-provider/lib/NftErc1155Provider.ts
+++ packages/nft-erc1155-provider/lib/NftErc1155Provider.ts
@@ -1,28 +1,18 @@
-import { Provider } from '@liquality/provider'
 import { addressToString } from '@liquality/utils'
 import { ensure0x, normalizeTransactionObject } from '@liquality/ethereum-utils'
-import { NftProvider, Address } from '@liquality/types'
+import { Address } from '@liquality/types'
 import { StandardError } from '@liquality/errors'
+import { NftBaseProvider } from '@liquality/nft-base-provider'
 
 import { isArray } from 'lodash'
-import { Contract } from '@ethersproject/contracts'
 import { Signer } from '@ethersproject/abstract-signer'
 
 import NftErc1155_ABI from './NftErc1155_ABI.json'
-
 const erc1155InterfaceID = '0xd9b67a26'
 
-export default class NftErc1155Provider extends Provider implements Partial<NftProvider> {
-  _contract: Contract
-  _signer: Signer
-  _contractCache: { [address: string]: boolean }
-
+export default class NftErc1155Provider extends NftBaseProvider {
   constructor(signer: Signer) {
-    super()
-
-    this._signer = signer
-    this._contract = new Contract('0x0000000000000000000000000000000000000000', NftErc1155_ABI, this._signer)
-    this._contractCache = {}
+    super(signer, NftErc1155_ABI as any, erc1155InterfaceID)
   }
 
   async balance(contract: Address | string, tokenIDs: number | number[]) {
@@ -30,7 +20,8 @@ export default class NftErc1155Provider extends Provider implements Partial<NftP
       return 0
     }
 
-    await this._attach(contract)
+    await super.balance(contract, tokenIDs)
+
     const owner = await this._signer.getAddress()
 
     if (isArray(tokenIDs)) {
@@ -50,7 +41,7 @@ export default class NftErc1155Provider extends Provider implements Partial<NftP
     values: number[],
     data = '0x00'
   ) {
-    await this._attach(contract)
+    await super.transfer(contract, receiver, tokenIDs, values, data)
 
     if (isArray(tokenIDs) && isArray(values) && tokenIDs.length === values.length) {
       let txWithHash
@@ -78,47 +69,19 @@ export default class NftErc1155Provider extends Provider implements Partial<NftP
   }
 
   async approveAll(contract: Address | string, receiver: Address | string, state = true) {
-    await this._attach(contract)
+    await super.approveAll(contract, receiver, state)
 
     const txWithHash = await this._contract.functions.setApprovalForAll(ensure0x(addressToString(receiver)), state)
     return normalizeTransactionObject(txWithHash)
   }
 
   async isApprovedForAll(contract: Address | string, operator: Address | string) {
-    await this._attach(contract)
+    await super.isApprovedForAll(contract, operator)
 
     const state = await this._contract.functions.isApprovedForAll(
       await this._signer.getAddress(),
       ensure0x(addressToString(operator))
     )
     return state[0]
-  }
-
-  private async _supportsInterface(contractInstance: Contract) {
-    const contractAddress = contractInstance.address.toLowerCase()
-
-    if (this._contractCache[contractAddress] !== undefined) {
-      return this._contractCache[contractAddress]
-    }
-
-    const state = await contractInstance.functions.supportsInterface(erc1155InterfaceID)
-    this._contractCache[contractAddress] = state[0] // store in cache
-    return state[0]
-  }
-
-  private async _attach(contract: Address | string) {
-    const _contractAddress = ensure0x(addressToString(contract))
-
-    if (this._contract.address !== _contractAddress) {
-      const contractInstance = this._contract.attach(_contractAddress)
-      // contract validation
-      if (await this._supportsInterface(contractInstance)) {
-        this._contract = contractInstance
-      } else {
-        throw new StandardError(
-          `Contract on address: ${_contractAddress} does not support EIP1155 interface (id: ${erc1155InterfaceID})`
-        )
-      }
-    }
   }
 }
