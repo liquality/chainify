51034fd31c9a57cc3769af3a685ba70f55a0585f
Krasimir Raykov <raykov@weichain.tech>
2022-03-30 16:45:52 +0300

    feat: add EvmChainProvider docs

--- packages/evm/lib/chain/EvmChainProvider.ts
+++ packages/evm/lib/chain/EvmChainProvider.ts
@@ -6,9 +6,31 @@ import { EthersBlock, EthersBlockWithTransactions, EthersTransactionResponse } f
 import { calculateFee, parseBlockResponse, parseTxResponse } from '../utils';
 import { EvmMulticallProvider } from './EvmMulticallProvider';
 
+/**
+ * Represents a connection to any EVM network.
+ * Used to fetch chain specific data like blocks, transactions, balances and fees. It uses {@link https://docs.ethers.io/v5/api | ethersjs}
+ *
+ * Example:
+ * ```typescript
+ * import { providers } from 'ethers';
+ * import { EvmNetworks, EvmChainProvider } from '@liquality/evm';
+ *
+ * const provider = new providers.StaticJsonRpcProvider(EvmNetworks.ganache.rpcUrl);
+ * const chainProvider = new EvmChainProvider(EvmNetworks.ganache, provider, null);
+ * ```
+ * @public
+ *
+ */
 export class EvmChainProvider extends Chain<StaticJsonRpcProvider> {
     protected multicall: EvmMulticallProvider;
 
+    /**
+     * @param network - See {@link EvmNetworks}
+     * @param provider - Instance of {@link https://docs.ethers.io/v5/api/providers/jsonrpc-provider/#StaticJsonRpcProvider | StaticJsonRpcProvider}
+     * If not passed, it's created internally based on the `rpcUrl` from the network parameter.
+     * @param feeProvider - Instance of {@link Fee}.
+     * If not passed, it uses {@link https://docs.ethers.io/v5/api/providers/provider/#Provider-getFeeData | getFeeData} from the ethers provider.
+     */
     constructor(network: Network, provider?: StaticJsonRpcProvider, feeProvider?: Fee) {
         super(network, provider, feeProvider);
 
@@ -19,6 +41,14 @@ export class EvmChainProvider extends Chain<StaticJsonRpcProvider> {
         this.multicall = new EvmMulticallProvider(this.provider, Number(network.chainId));
     }
 
+    /**
+     * @param blockHash - the hash of the block
+     * @returns
+     * If `includeTx` is `false` the `_raw` object type is {@link https://docs.ethers.io/v5/api/providers/types/#providers-Block | EthersBlock}.
+     *
+     * If `includeTx` is `true`  the `_raw` object type {@link https://docs.ethers.io/v5/api/providers/types/#providers-BlockWithTransactions | BlockWithTransactions}
+     * and `transactions` array with type {@link https://docs.ethers.io/v5/api/providers/types/#providers-TransactionResponse | EthersTransactionResponse}
+     */
     public async getBlockByHash(
         blockHash: string,
         includeTx = false
@@ -26,6 +56,14 @@ export class EvmChainProvider extends Chain<StaticJsonRpcProvider> {
         return this._getBlock(blockHash, includeTx);
     }
 
+    /**
+     * @param blockNumber - the number of the block. If not passed, it fetches the latest block
+     * @returns
+     * If `includeTx` is `false` the `_raw` object type is {@link https://docs.ethers.io/v5/api/providers/types/#providers-Block | EthersBlock}.
+     *
+     * If `includeTx` is `true`  the `_raw` object type {@link https://docs.ethers.io/v5/api/providers/types/#providers-BlockWithTransactions | BlockWithTransactions}
+     * and `transactions` array with type {@link https://docs.ethers.io/v5/api/providers/types/#providers-TransactionResponse | EthersTransactionResponse}
+     */
     public async getBlockByNumber(
         blockNumber?: number,
         includeTx = false
@@ -36,10 +74,18 @@ export class EvmChainProvider extends Chain<StaticJsonRpcProvider> {
         return this._getBlock(blockNumber, includeTx);
     }
 
+    /**
+     * @returns resolves with the latest block number
+     */
     public async getBlockHeight(): Promise<number> {
         return this.provider.getBlockNumber();
     }
 
+    /**
+     * @param txHash - the transaction hash prefixed with `0x`
+     * @returns a transaction object with _raw object type
+     * of {@link https://docs.ethers.io/v5/api/providers/types/#providers-TransactionResponse | EthersTransactionResponse}
+     */
     public async getTransactionByHash(txHash: string): Promise<Transaction<EthersTransactionResponse>> {
         const tx = await this.provider.getTransaction(txHash);
         const result = parseTxResponse(tx);
@@ -52,16 +98,28 @@ export class EvmChainProvider extends Chain<StaticJsonRpcProvider> {
         return result;
     }
 
+    /**
+     * @param addresses - currently fetches the balances only for `addresses[0]`
+     * @param assets - the list of assets
+     * @returns - the balances of `assets` in the passed order
+     */
     public async getBalance(addresses: AddressType[], assets: Asset[]): Promise<BigNumber[]> {
         const balances = await this.multicall.getMultipleBalances(addresses[0], assets);
         return balances.map((b) => new BigNumber(b.toString()));
     }
 
+    /**
+     * @param rawTransaction - the signed transaction
+     * @returns the hash of the transaction
+     */
     public async sendRawTransaction(rawTransaction: string): Promise<string> {
         const tx = await this.provider.sendTransaction(rawTransaction);
         return tx.hash;
     }
 
+    /**
+     * If the `feeProvider` is not defined, it fetches the fees from {@link https://docs.ethers.io/v5/api/providers/provider/#Provider-getFeeData | getFeeData}
+     */
     public async getFees(): Promise<FeeDetails> {
         if (this.feeProvider) {
             return this.feeProvider.getFees();
@@ -76,6 +134,9 @@ export class EvmChainProvider extends Chain<StaticJsonRpcProvider> {
         }
     }
 
+    /**
+     * @throws {@link UnsupportedMethodError} if the ethers provider doesn't support rpc calls
+     */
     public async sendRpcRequest(method: string, params: any[]): Promise<any> {
         if (!this.provider.send) {
             throw new UnsupportedMethodError('Method not supported.');
