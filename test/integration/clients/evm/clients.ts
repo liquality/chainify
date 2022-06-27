6e8243e8daf4dc219dcfd7df144ead3ff6183b26
Krasimir Raykov <raykov@weichain.tech>
2022-06-27 13:42:00 +0300

    feat: eip1559 provider (#566)
    * feat: eip1559 provider
    
    * fix: remove avalanche for now
    
    * fix: revert tests back to mock fee provider

--- test/integration/clients/evm/clients.ts
+++ test/integration/clients/evm/clients.ts
@@ -11,7 +11,7 @@ function getEvmClient(network: Network) {
     const config = EVMConfig(network);
     const provider = new providers.StaticJsonRpcProvider(network.rpcUrl);
     // using mainnet gas fees
-    const feeProvider = new EVM.EIP1559FeeApiProvider(EVM.EvmNetworks.ethereum_mainnet.chainId);
+    const feeProvider = new EIP1559MockFeeProvider(provider);
     const chainProvider = new EVM.EvmChainProvider(network, provider, feeProvider);
     // we don't have multicall on the common address on Ganache
     void chainProvider.multicall.setMulticallAddress('0x08579f8763415cfCEa1B0F0dD583b1A0DEbfBe2b');
